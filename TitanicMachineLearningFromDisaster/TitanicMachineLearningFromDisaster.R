5e2b936cc2b9eb79dff48efcd1d95faa39366d37
yauheni-se <likedubaipolice@gmail.com>
2021-10-30 01:20:54 +0200

    TitanicFromDisasterEugeneS R script added

---
+++ TitanicMachineLearningFromDisaster/TitanicMachineLearningFromDisaster.R
@@ -0,0 +1,48 @@
+#####
+# SETUP
+#####
+
+library(dplyr)
+library(data.table)
+library(ggplot2)
+
+rm(list=ls())
+
+train <- data.table::fread("train.csv")
+
+#####
+# EDA
+#####
+
+
+# Structure:
+train
+train[, vapply(.SD, class, character(1))]
+train[, vapply(.SD, uniqueN, numeric(1))]   # Name, PassengerId, Ticket(?) are useless vars for prediction
+
+
+# NA's:
+train[, vapply(.SD, anyNA, logical(1))]
+naniar::vis_miss(train)
+
+
+# Statistics:
+
+summary(train)
+
+train %>% 
+  group_by(Pclass) %>% 
+  summarize(SurvivalRate = mean(Survived))
+
+train %>% 
+  group_by(Survived) %>% 
+  summarize(mean(Fare))
+
+train %>% 
+  group_by(SibSp) %>% 
+  summarize(SurvivalRate = mean(Survived))
+
+train %>% 
+  group_by(Sex) %>% 
+  summarize(SurvivalRate = mean(Survived))
+
